b2b11c329b0bd73e698db77b45d4752a3892da90
Marwa Alkuwari <160133098+Alkuwari91@users.noreply.github.com>
2025-12-29 22:13:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(BASE_DIR, "data", "universities.csv")
+
+df = pd.read_csv(DATA_PATH)
 import streamlit as st
 import pandas as pd
 from pathlib import Path
